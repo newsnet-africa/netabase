e1f4f0d81558a718b317c473c24e0da9a7264b2f
Nzuzo Magagula <nzuzo@newsnet.africa>
2025-07-31 11:07:04 +0200

    I think teh netabase lib was weird?

--- netabase_macros/src/lib.rs
+++ netabase_macros/src/lib.rs
@@ -4,12 +4,17 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{ItemMod, ItemStruct, Path, PathSegment, parse_macro_input, visit::Visit};
 
+<<<<<<< HEAD
 use crate::visitors::{schema_finder::SchemaFinder, schema_validator::SchemaValidator};
+=======
+use crate::visitors::{schema_finder::SchemaFinder, utils::KeyType};
+>>>>>>> 9ebb163c7b1984ab70d5bbe2ab7aa48824850724
 
 mod generators;
 mod visitors;
 
 #[proc_macro_attribute]
+<<<<<<< HEAD
 pub fn schema(_: TokenStream, module1: TokenStream) -> TokenStream {
     let module1 = parse_macro_input!(module1 as ItemMod);
     let id = module1.ident.clone();
@@ -23,6 +28,21 @@ pub fn schema(_: TokenStream, module1: TokenStream) -> TokenStream {
     quote! {
         fn list(){
             #(#schemas);*;
+=======
+pub fn schema(_: TokenStream, module: TokenStream) -> TokenStream {
+    let module = parse_macro_input!(module as ItemMod);
+    let id = module.ident.clone();
+    let mut finder = SchemaFinder::default();
+    finder.visit_item_mod(&module);
+    let schemas = finder
+        .schemas
+        .iter()
+        .map(|i| i.schema_key.clone().unwrap().generation_type.unwrap())
+        .collect::<Vec<KeyType<'_>>>();
+    quote! {
+        fn foo() {
+            stringify!(#(#schemas)*);
+>>>>>>> 9ebb163c7b1984ab70d5bbe2ab7aa48824850724
         }
     }
     .into()
