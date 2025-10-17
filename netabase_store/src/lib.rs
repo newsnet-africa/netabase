1804d0e9ea18d5ed863608afbcd8ce350d464126
nzuzo@newsnet.africa <nzuzo@newsnet.africa>
2025-10-18 00:06:51 +0200

    broken

--- netabase_store/src/lib.rs
+++ netabase_store/src/lib.rs
@@ -1,3 +1,9 @@
 pub mod databases;
 pub mod error;
 pub mod traits;
+
+// Re-export netabase_deps for users of the macros
+pub use netabase_deps::*;
+
+// Re-export macros for convenience
+pub use netabase_macros::*;
